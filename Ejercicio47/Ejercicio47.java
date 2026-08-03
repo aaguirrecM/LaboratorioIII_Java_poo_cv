fa9586af1f4173ee05d22d49cc45c81db8f88276
Andrea <287881827+aaguirrecM@users.noreply.github.com.>
2026-08-03 00:41:28 -0600

    feat: implementa ejercicio47

---
+++ Ejercicio47/Ejercicio47.java
@@ -0,0 +1,8 @@
+package Ejercicio47;
+public class Ejercicio47 {
+    public static void main(String[] args) {
+        Empleado e = new Empleado();
+        e.setSalario(4215);
+        System.out.println("Salario encapsulado: Q" + e.getSalario());
+    }
+}
