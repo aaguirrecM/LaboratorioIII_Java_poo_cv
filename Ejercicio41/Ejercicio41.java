42919f9d64d531f76b431aedb59f262c664dc30e
Andrea <287881827+aaguirrecM@users.noreply.github.com.>
2026-08-02 23:55:17 -0600

    feat: implementa ejercicio41

---
+++ Ejercicio41/Ejercicio41.java
@@ -0,0 +1,8 @@
+package Ejercicio41;
+public class Ejercicio41 {
+    public static void main(String[] args) {
+        Estudiante e = new Estudiante();
+        e.nombre = "Emilu";
+        System.out.println("Clase Estudiante creada para: " + e.nombre);
+    }
+}
