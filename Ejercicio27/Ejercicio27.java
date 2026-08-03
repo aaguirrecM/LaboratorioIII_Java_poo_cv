d0ac2ddb7a402846aa8ec8d4a8fe3830e585f126
Andrea <287881827+aaguirrecM@users.noreply.github.com.>
2026-08-02 22:31:00 -0600

    feat: cambio en el ejercicio27

--- Ejercicio27/Ejercicio27.java
+++ Ejercicio27/Ejercicio27.java
@@ -1,18 +1,9 @@
+package Ejercicio27;
 public class Ejercicio27 {
     public static void main(String[] args) {
-        Cuenta cuenta = new Cuenta("Sabrina Spellman", 1000.00);
-        cuenta.mostrarSaldo();
-
-        cuenta.depositar(500.00);
-        cuenta.mostrarSaldo();
-
-        try {
-            cuenta.retirar(2000.00); 
-        } catch (IllegalArgumentException e) {
-            System.out.println("Error en transacción: " + e.getMessage());
-        }
-
-        cuenta.retirar(300.00);
-        cuenta.mostrarSaldo();
+        Persona p = new Persona();
+        p.setNombre("Luis");
+        p.setEdad(20);
+        System.out.println(p.getNombre() + " tiene " + p.getEdad());
     }
 }
