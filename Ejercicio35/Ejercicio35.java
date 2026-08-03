9739bfc827f4df4d8f8ea2fa416b9c8131c0c7e1
Andrea <287881827+aaguirrecM@users.noreply.github.com.>
2026-08-02 23:31:01 -0600

    feat: implementa ejercicio35

---
+++ Ejercicio35/Ejercicio35.java
@@ -0,0 +1,8 @@
+package Ejercicio35;
+public class Ejercicio35 {
+    public static void main(String[] args) {
+        Cuenta c = new Cuenta(300.0);
+        c.depositar(100.0);
+        c.mostrarSaldo();
+    }
+}
