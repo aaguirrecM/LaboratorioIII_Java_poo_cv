cbd7de454aa1a8a0bfa0528c2819bd054b33acca
Andrea <287881827+aaguirrecM@users.noreply.github.com.>
2026-08-02 23:22:21 -0600

    feat: implementa nuevo ejericio31

--- Ejercicio31/Ejercicio31.java
+++ Ejercicio31/Ejercicio31.java
@@ -1,7 +1,7 @@
 package Ejercicio31;
 public class Ejercicio31 {
     public static void main(String[] args) {
-        Cuenta c = new Cuenta(100.0);
-        System.out.println("Cuenta creada.");
+        Cuenta cuenta = new Cuenta(100.0);
+        System.out.println("Cuenta creada con éxito. Saldo inicial: Q" + cuenta.getSaldo());
     }
 }
