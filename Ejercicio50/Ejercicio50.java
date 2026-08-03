69697a1aefc86174f0f2d2d53d71215cc66b994d
Andrea <287881827+aaguirrecM@users.noreply.github.com.>
2026-08-03 00:53:46 -0600

    feat: implementa ejercicio50

---
+++ Ejercicio50/Ejercicio50.java
@@ -0,0 +1,12 @@
+package Ejercicio50;
+public class Ejercicio50 {
+    public static void main(String[] args) {
+        Empleado e1 = new Empleado("Ian", 4500);
+        Empleado e2 = new Empleado("Melissa", 6000);
+        Empleado e3 = new Empleado("Alisson", 9000);
+        
+        e1.mostrarDatos();
+        e2.mostrarDatos();
+        e3.mostrarDatos();
+    }
+}
---
+++ Ejercicio50/Empleado.java
@@ -0,0 +1,15 @@
+package Ejercicio50;
+public class Empleado {
+    private String nombre;
+    private double salario;
+
+    public Empleado(String nombre, double salario) {
+        this.nombre = nombre;
+        this.salario = salario;
+    }
+
+    public void mostrarDatos() {
+        System.out.println("Empleado: " + nombre );
+        System.out.println(" | Salario: Q" + salario);
+    }
+}
